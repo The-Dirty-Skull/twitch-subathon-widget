d8f929f0f13f7e16dc7bc39ba8d9071c3478cb0b
Jinjer88 <mojtaba.etebari@gmail.com>
2025-09-02 10:41:59 +0330

    Fixed total sub point error

--- Timer Widget/JS.js
+++ Timer Widget/JS.js
@@ -313,5 +313,5 @@ function showNextMessage() {
 
 function updateSubCount(data) {
     const subCountEl = document.getElementById('subCount');
-    subCountEl.textContent = `Subs: ${data["subscriber-points"]["count"]}`;
+    subCountEl.textContent = `Subs: ${data["subscriber-points"]["amount"]}`;
 }
